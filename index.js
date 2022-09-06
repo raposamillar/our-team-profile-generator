fd80acfe5332e5cce1f3569605377677a4a0c295
raposamillar <lisa.raposamillar@gmail.com>
2022-09-05 21:53:31 -0400

    finish questions, add styling, and simplify directory structure

--- index.js
+++ index.js
@@ -1,32 +1,4 @@
 const { prompt } = require('inquirer');
+const { questions, htmlGen } = require('./lib');
 
-prompt([
-  {
-    type: "confirm",
-    name: "fillPosition",
-    message: "Would you like to fill a position?"
-  },
-  {
-    type: "list",
-    name: "role",
-    message: "What position would you like to fill?",
-    choices: ["Manager", "Engineer", "Intern"],
-    when: ({fillPosition}) => fillPosition
-  },
-  {
-    type: "input",
-    name: "name",
-    message: "What is the employee's name?"
-  },
-  {
-    type: "input",
-    name: "id",
-    message: "What is the employee's id?"
-  },
-  {
-    type: "input",
-    name: "email",
-    message: "What is the employee's email?"
-  }
-])
-
+prompt(questions).then(htmlGen);
